616cb6199ba213ac3991229539a6e8cb71c94acf
sdpb <sdpb000@gmail.com>
2019-08-18 21:50:08 -0500

    Simulacro de proyecto

--- laboratorios/lab01/codigo/PuertoUraba.java
+++ laboratorios/lab01/codigo/PuertoUraba.java
@@ -3,14 +3,6 @@ package Laboratorio_1;
 public class PuertoUraba {
 
     public static int combinaciones(int n){
-        return (n <= 3) ? n : combinaciones(n - 1) + ( n - 2);
-        /*
-        if (n <= 3) return  n; //c_1
-        return combinaciones(n - 1) + (n - 2); // T(n) = T(n-1) + n + c_2
-        */
-
+        return (n <= 3) ? n : combinaciones(n - 1) + (n - 2);
     }
-
-
-
 }
