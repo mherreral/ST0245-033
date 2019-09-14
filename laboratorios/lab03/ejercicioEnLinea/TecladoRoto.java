629a11272351a1fe9a492d6b70d6262a8888e172
mherreral <53027815+mherreral@users.noreply.github.com>
2019-09-14 10:51:58 -0500

    Add files via upload

---
+++ laboratorios/lab03/ejercicioEnLinea/TecladoRoto.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manuh
+ */
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TecladoRoto {
+
+    public static void main(String[] args) {
+        System.out.println("Escriba el input");
+        Scanner scanner = new Scanner(System.in);
+        String caracteres = scanner.next();
+        
+        LinkedList<Character> linkedList = new LinkedList<>();
+        
+        System.out.println("Escriba 'EOL' para ver cómo sería el output");
+        
+        while (scanner.next().equals("EOL")) {
+            int voy = 0;
+            for (char caracter : caracteres.toCharArray()) {
+                switch (caracter) {
+                    case '[':
+                        voy = 0;
+                        break;
+                    case ']':
+                        voy = linkedList.size();
+                        break;
+                    default:
+                        linkedList.add(voy++, caracter);
+                        break;
+                }
+            }
+            linkedList.forEach((caracter) -> {
+                System.out.print(caracter);
+            });
+            break;
+        }
+    }
+}
