e2f27fb85010932f2c26bf1ac9966d342bf094da
sdpb <sdpb000@gmail.com>
2019-10-06 17:41:03 -0500

    Arbol de decision

--- proyecto/codigo/ArbolDeDecision.py
+++ proyecto/codigo/ArbolDeDecision.py
@@ -3,14 +3,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
 
 
-data = pd.read_csv('../DataSets/train/data_set_train.csv') # Aquí se lee el data_set
+data = pd.read_csv('./data_set_balanced.csv') # Aquí se lee el data_set
 
 print(data.count(0) / data.shape[0] * 100) # Esto imprime el porcentaje de datos que hay en cada columna (100% significa que no hay datos vacios)
 
 print("\n", data.groupby('label').label.count()) # Imprime la que cantidad de datos contienen un valor de label
 
 features = ['ph', 'soil_temperature', 'soil_moisture', 'illuminance',
-            'env_temperature', 'env_humidity']                          # Variables dependientes
+            'env_temperature', 'env_humidity']                          # Variables independientes
 
 X = data[features].values # Se almacenan los valores de las variables independientes
 
@@ -18,7 +18,7 @@ y = data['label'].values # Se almacenan los valores de la variable dependiente
 
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=100)
 
-clf = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=4, min_samples_leaf=5)
+clf = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=5, min_samples_leaf=5)
 # Esto genera el árbol con una frofundidad de maximo 5 niveles o un criterio de minimo 5 datos para dejar de generar nodos
 
 clf.fit(X, y) # Esto lo entrena
